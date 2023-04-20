7791f51ec94362e3d34d83fe3513659007d79a1b
leemj0948 <ljo094822@naver.com>
2023-04-20 17:50:33 +0900

    add: chatGPT 가 이전의 내용을 기억하도록 기억 내용 기록 및 api요청 로직 추가

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,37 @@ app.use(cors());
 //post call
 app.use(express.json()); // parsing application/json
 app.use(express.urlencoded({ extended: true })); //parsing application/x-www
-
+let messages = [
+    {
+        role: "system",
+        content:
+            "당신은 10000편 이상의 영화를 시청하였으며 영화평론가 이자 추천가입니다. 당신은 user의 성향에 따라 알맞은 영화의 장르 및 영화를 추천해줄수 있습니다.",
+    },
+    {
+        role: "user",
+        content:
+            "당신은 천만편 이상의 영화를 시청하였으며 영화평론가입니다. 당신은 나의 성향에 따라 알맞은 영화 장르 및 영화를 추천해줄수 있습니다.",
+    },
+    {
+        role: "assistant",
+        content:
+            "네 저는 세계에서 알아주는 영화 평론가이며 천만편 이상의 영화를 시청하였습니다. 당신에게 영화를 추천해드리겠습니다. 당신의 취향을 알려주시겠어요? 가능하다면 마지막에 본 영화와 그 영화에 대한 짧은 생각도 함께 부탁드립니다.",
+    },
+];
 app.post("/moviebot", async function (req, res) {
-    const answer = await callApi();
+    messages = [...messages, { role: "user", content: req.body.user }];
+    const answer = await callApi(messages);
     // res.send(answer);
-    console.log("answer :", answer);
+    console.log("요청", req.body);
+    console.log("메시징", messages);
+    console.log("결과", answer);
+    messages = [
+        ...messages,
+        {
+            role: "assistant",
+            content: answer,
+        },
+    ];
     res.json({ assistant: answer });
 });
 app.listen(3000);
@@ -35,29 +61,9 @@ const callApi = async () => {
     try {
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            messages: [
-                {
-                    role: "system",
-                    content:
-                        "당신은 10000편 이상의 영화를 시청하였으며 영화평론가 이자 추천가입니다. 당신은 user의 성향에 따라 알맞은 영화의 장르 및 영화를 추천해줄수 있습니다.",
-                },
-                {
-                    role: "user",
-                    content:
-                        "당신은 천만편 이상의 영화를 시청하였으며 영화평론가입니다. 당신은 나의 성향에 따라 알맞은 영화 장르 및 영화를 추천해줄수 있습니다.",
-                },
-                {
-                    role: "assistant",
-                    content:
-                        "네 저는 세계에서 알아주는 영화 평론가이며 천만편 이상의 영화를 시청하였습니다. 당신에게 영화를 추천해드리겠습니다. 당신의 취향을 알려주시겠어요? 가능하다면 마지막에 본 영화와 그 영화에 대한 짧은 생각도 함께 부탁드립니다.",
-                },
-                {
-                    role: "user",
-                    content:
-                        "저는 '탑건'을 가장 재미있게 봤으며 영화의 웅장함과 영상미가 아주 좋았습니다. 액션 쪽 장르를 선호합니다. ",
-                },
-            ],
+            messages: messages,
         });
+
         return completion.data.choices[0].message["content"];
     } catch (err) {
         console.log(err);
