b3a48fdb2fcd280ea718e38af2478c43bb26da77
leemj0948 <ljo094822@naver.com>
2023-05-03 13:02:37 +0900

    add: RESTAPI 가 배포과정에서 안되어서 수정, 직접 주소 입력

--- src/components/ChatMain.tsx
+++ src/components/ChatMain.tsx
@@ -30,7 +30,9 @@ const ChatMain = () => {
     }, [chatbot]);
 
     const callApi = async (): Promise<string | undefined> => {
-        const RESTAPI = import.meta.env.VITE_RESTAPI;
+        const RESTAPI =
+            import.meta.env.VITE_RESTAPI ??
+            "https://b327hv97gk.execute-api.ap-southeast-2.amazonaws.com/prod";
         console.log(RESTAPI);
         try {
             const res = await axios.post(`${RESTAPI}/moviebot`, {
